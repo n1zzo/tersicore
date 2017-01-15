bc9bd10eb9b8d704099584f2f94bf37c78fa0fb7
Niccolò Izzo <izzo.niccolo@gmail.com>
2017-01-15 12:59:39 +0100

    Updated API and fixed minor stuff.

--- DB.py
+++ DB.py
@@ -1,27 +1,33 @@
 import configparser
 import pyodbc
 
-# class DB:
-#     def connect()
-#     def init()
-#     def add_track(tag) -> ID
-#     def update_track(tag) -> ID
-#     def get_path(ID) -> path
-#     def get_tag(ID) -> tag
-#     def search_keyword(keyword) -> IDlist
+#class DB:
+#    def _init()
+#    def add_track(path, tag) -> ID
+#    def update_track(ID, tag)
+#    def get_path(ID) -> path
+#    def get_tag(ID) -> tag
+#    def search_keyword(keyword) -> IDlist
 
 class DB:
     cnxn = None
 
     def __init__(self):
         # Parse configuration file and generate connection string
+        config = configparser.ConfigParser()
         config.read('config.ini')
+
         driver = config['DATABASE']['Driver']
         server = config['DATABASE']['Server']
         port = config['DATABASE']['Port']
         database = config['DATABASE']['Database']
         uid = config['DATABASE']['UID']
         password = config['DATABASE']['Password']
+
         conn_string = ('DRIVER='+driver+';SERVER='+server+';PORT='+port+
                      ';DATABASE='+database+';UID='+uid+';PWD='+password)
         self.cnxn = pyodbc.connect(conn_string)
+
+if __name__ == "__main__":
+    db = DB()
+
