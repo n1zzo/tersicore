b731cc4d90d091ffee0f15a31396efbfe222fe77
Niccolò Izzo <izzo.niccolo@gmail.com>
2017-01-15 14:12:29 +0100

    Refactored DB class.

--- DB.py
+++ DB.py
@@ -3,6 +3,7 @@ import pyodbc
 
 #class DB:
 #    def _init()
+#    def connect()
 #    def add_track(path, tag) -> ID
 #    def update_track(ID, tag)
 #    def get_path(ID) -> path
@@ -10,7 +11,9 @@ import pyodbc
 #    def search_keyword(keyword) -> IDlist
 
 class DB:
+    conn_string = None
     cnxn = None
+    cursor = None
 
     def __init__(self):
         # Parse configuration file and generate connection string
@@ -21,13 +24,22 @@ class DB:
         server = config['DATABASE']['Server']
         port = config['DATABASE']['Port']
         database = config['DATABASE']['Database']
+        socket = config['DATABASE']['Socket']
         uid = config['DATABASE']['UID']
         password = config['DATABASE']['Password']
 
-        conn_string = ('DRIVER='+driver+';SERVER='+server+';PORT='+port+
-                     ';DATABASE='+database+';UID='+uid+';PWD='+password)
-        self.cnxn = pyodbc.connect(conn_string)
+        self.conn_string = ('DRIVER='+driver+';SERVER='+server+
+                            ';PORT='+port+';DATABASE='+database+
+                            ';SOCKET='+socket+';UID='+uid+';PWD='+password)
+
+    def connect(self):
+        self.cnxn = pyodbc.connect(self.conn_string)
+        self.cursor = self.cnxn.cursor()
+
+        self.cnxn.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-8')
+        self.cnxn.setencoding(encoding='utf-8')
 
 if __name__ == "__main__":
     db = DB()
+    db.connect()
 
