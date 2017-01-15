602fb37a51d23103047b39d31b8929b2b43a9e24
Edoardo Negri <edne@gmx.com>
2017-01-15 17:03:15 +0100

    Load configuration

--- config.py
+++ config.py
@@ -1,27 +1,25 @@
-import configparser
+from configparser import ConfigParser
+import os.path
 
-class Config:
-    DEFAULT_PATHS = ('config.ini', '/etc/antani/config.ini')
+DEFAULT_PATHS = 'config.ini', '/etc/musiclibrary/config.ini'
 
-    path   = None
-    config = configparser.ConfigParser()
 
-    def __init__(self, path = None):
-        if path == None:
-            for p in DEFAULT_PATHS:
-                if os.path.isfile(p):
-                    self.path = p
-                    break;
-        else:
-            self.path = path
-        try:
-            config.read(self.path)
-        except ConfigParser.Error:
-            # Handle it
-            return
+def get_config(path=None):
+    "Load the configuration"
+    config = ConfigParser()  # dict-like structure
+    if path:
+        config.read(path)
+    else:
+        for path in DEFAULT_PATHS:
+            if os.path.isfile(path):
+                config.read(path)
+                break
+        else:  # no break
+            print("Config not found")  # TODO: logger
+            # raise an exception?
+    return config
 
-    def reload():
-        try:
-            config.read(self.path)
-        except ConfigParser.Error:
-            return
+
+# test
+if __name__ == "__main__":
+    print(dict(get_config()))
