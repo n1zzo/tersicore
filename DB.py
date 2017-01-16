3c42044478a4f9ba3e758ac33485129782dc83c7
Tancredi Orlando <turlando@gmail.com>
2017-01-16 11:13:47 +0100

    Improved again queries legibility

--- DB.py
+++ DB.py
@@ -13,60 +13,67 @@ import uuid
 #     def get_tag(ID) -> tag
 #     def search_keyword(keyword) -> IDlist
 
-CREATE_USERS = ("CREATE TABLE Users ("
-                "ID BIGINT NOT NULL AUTO_INCREMENT, "
-                "username VARCHAR(256) NOT NULL, "
-                "pw_hash BINARY(64) NOT NULL, "
-                "pw_salt BINARY(64) NOT NULL, "
-                "PRIMARY KEY(ID))")
 
-CREATE_TRACKS = ("CREATE TABLE Tracks ("
-                 "UUID BINARY(16) NOT NULL, "
-                 "track_number INTEGER, "
-                 "total_tracks INTEGER, "
-                 "disc_number INTEGER, "
-                 "total_discs INTEGER, "
-                 "title VARCHAR(256) NOT NULL, "
-                 "artist VARCHAR(256) NOT NULL, "
-                 "album_artist VARCHAR(256), "
-                 "date VARCHAR(256), "
-                 "label VARCHAR(256), "
-                 "ISRC VARCHAR(256), "
-                 "PRIMARY KEY(UUID))")
+class Database:
+    QUERY_CREATE_TABLE_USERS = (
+            "CREATE TABLE  Users ("
+                "ID        BIGINT        NOT NULL  AUTO_INCREMENT,"
+                "username  VARCHAR(256)  NOT NULL,"
+                "pw_hash   BINARY(64)    NOT NULL,"
+                "pw_salt   BINARY(64)    NOT NULL,"
+                "PRIMARY KEY(ID));")
 
-CREATE_LIBRARIES = ("CREATE TABLE Libraries ("
-                    "UUID BINARY(16) NOT NULL, "
-                    "base_dir VARCHAR(256) NOT NULL, "
-                    "owner BIGINT NOT NULL, "
-                    "PRIMARY KEY(UUID), "
-                    "FOREIGN KEY(owner) REFERENCES Users(ID))")
+    QUERY_CREATE_TABLE_TRACKS = (
+            "CREATE TABLE Tracks ("
+                "UUID          BINARY(16)    NOT NULL,"
+                "track_number  INTEGER,"
+                "total_tracks  INTEGER,"
+                "disc_number   INTEGER,"
+                "total_discs   INTEGER,"
+                "title         VARCHAR(256)  NOT NULL,"
+                "artist        VARCHAR(256)  NOT NULL,"
+                "album_artist  VARCHAR(256),"
+                "date          VARCHAR(256),"
+                "label         VARCHAR(256),"
+                "ISRC          VARCHAR(256),"
+                "PRIMARY KEY(UUID));")
 
-CREATE_RESOURCES = ("CREATE TABLE Resources ("
-                    "UUID BINARY(16) NOT NULL, "
-                    "track_UUID BINARY(16) NOT NULL, "
-                    "last_modified TIMESTAMP NOT NULL, "
-                    "codec SMALLINT NOT NULL, "
-                    "bitrate SMALLINT NOT NULL, "
-                    "owner BIGINT NOT NULL, "
-                    "path VARCHAR(256) NOT NULL, "
-                    "PRIMARY KEY(UUID), "
-                    "FOREIGN KEY(track_UUID) REFERENCES Tracks(UUID))")
+    QUERY_CREATE_TABLE_LIBRARIES = (
+            "CREATE TABLE Libraries ("
+                "UUID      BINARY(16)    NOT NULL,"
+                "base_dir  VARCHAR(256)  NOT NULL,"
+                "owner     BIGINT        NOT NULL,"
+                "PRIMARY KEY(UUID),"
+                "FOREIGN KEY(owner) REFERENCES Users(ID));")
 
-DROP_TABLES = ("DROP TABLE IF EXISTS Resources, Libraries, Tracks, Users;")
+    QUERY_CREATE_TABLE_RESOURCES = (
+            "CREATE TABLE Resources ("
+                "UUID           BINARY(16)    NOT NULL,"
+                "track_UUID     BINARY(16)    NOT NULL,"
+                "last_modified  TIMESTAMP     NOT NULL,"
+                "codec          SMALLINT      NOT NULL,"
+                "bitrate        SMALLINT      NOT NULL,"
+                "owner          BIGINT        NOT NULL,"
+                "path           VARCHAR(256)  NOT NULL,"
+                "PRIMARY KEY(UUID),"
+                "FOREIGN KEY(track_UUID) REFERENCES Tracks(UUID));")
 
-INSERT_TRACK = ("INSERT INTO Tracks("
-                "UUID, track_number, total_tracks, disc_number, total_discs, "
-                "title, artist, album_artist, date, label, ISRC) "
-                "values (UNHEX(?),?,?,?,?,?,?,?,?,?,?)")
+    QUERY_DROP_ALL_TABLES = (
+            "DROP TABLE IF EXISTS Resources, Libraries, Tracks, Users;")
 
-UPDATE_TRACK = ("UPDATE Tracks "
-                "SET track_number=?, total_tracks=?, disc_number=?, "
+    QUERY_INSERT_TRACK = (
+            "INSERT INTO Tracks("
+                "UUID, track_number, total_tracks, disc_number, total_discs,"
+                "title, artist, album_artist, date, label, ISRC)"
+            "VALUES (UNHEX(?),?,?,?,?,?,?,?,?,?,?);")
+
+    QUERY_UPDATE_TRACK = (
+            "UPDATE Tracks"
+            "SET track_number=?, total_tracks=?, disc_number=?,"
                 "total_discs=?, title=?, artist=?, album_artist=?,"
-                "date=?,label=?,ISRC=? "
-                "WHERE UUID = UNHEX(?);")
+                "date=?,label=?,ISRC=?"
+            "WHERE UUID = UNHEX(?);")
 
-
-class DB:
     conn_string = None
     cnxn = None
     cursor = None
@@ -98,19 +105,19 @@ class DB:
         self.cnxn.setencoding(encoding='utf-8')
 
     def create_tables(self):
-        self.cursor.execute(CREATE_USERS)
-        self.cursor.execute(CREATE_TRACKS)
-        self.cursor.execute(CREATE_LIBRARIES)
-        self.cursor.execute(CREATE_RESOURCES)
+        self.cursor.execute(QUERY_CREATE_TABLE_USERS)
+        self.cursor.execute(QUERY_CREATE_TABLE_TRACKS)
+        self.cursor.execute(QUERY_CREATE_TABLE_LIBRARIES)
+        self.cursor.execute(QUERY_CREATE_TABLE_RESOURCES)
         self.cursor.commit()
 
     def drop_tables(self):
-        self.cursor.execute(DROP_TABLES)
+        self.cursor.execute(QUERY_DROP_ALL_TABLES)
         self.cursor.commit()
 
     def add_track(self, tag):
         UUID = uuid.uuid4()
-        self.cursor.execute(INSERT_TRACK, UUID.hex, tag["track_number"],
+        self.cursor.execute(QUERY_INSERT_TRACK, UUID.hex, tag["track_number"],
                             tag["total_tracks"], tag["disc_number"],
                             tag["total_discs"], tag["title"], tag["artist"],
                             tag["album_artist"], tag["date"],
@@ -119,7 +126,7 @@ class DB:
         return UUID
 
     def update_track(self, UUID, tag):
-        self.cursor.execute(UPDATE_TRACK, tag["track_number"],
+        self.cursor.execute(QUERY_UPDATE_TRACK, tag["track_number"],
                             tag["total_tracks"], tag["disc_number"],
                             tag["total_discs"], tag["title"], tag["artist"],
                             tag["album_artist"], tag["date"],
