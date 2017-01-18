aced7804ac00c479ef6162e3f81849f14165957a
Niccolò Izzo <izzo.niccolo@gmail.com>
2017-01-18 22:32:03 +0100

    Added directory traversal and scanning. Updated requirements.

--- scanner.py
+++ scanner.py
@@ -1,5 +1,8 @@
 from config import get_config
 from time import sleep
+from os import walk
+from os.path import splitext
+
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
@@ -16,9 +19,34 @@ class Scanner(Observer):
         self.schedule(handler, path, recursive=True)
 
 
+class LibraryBuilder:
+
+    supported_exts = [".mp3", ".flac", ".ogg"]
+
+    def scanPath(self, path):
+        for root, dirs, files in walk(path):
+            if len(files) > 0:
+                self.addFiles(files, root)
+
+    def addFiles(self, files, path):
+        for track in files:
+            name, ext = splitext(path+track)
+            if ext in self.supported_exts:
+                print("Inserting file:")
+                print(name)
+                print("With extension:")
+                print(ext)
+                print("Having root:")
+                print(path)
+                # [TODO] Add file to db
+
 if __name__ == "__main__":
     conf = get_config()
     path = conf["SCANNER"]["path"]
+
+    builder = LibraryBuilder()
+    builder.scanPath(path)
+
     scanner = Scanner(path)
     scanner.start()
     try:
