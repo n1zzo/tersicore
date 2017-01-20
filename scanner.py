b55731c03fe93b3a1705a818c18e62f9ab0ee787
Tancredi Orlando <turlando@gmail.com>
2017-01-20 16:32:06 +0100

    Refactoring

--- scanner.py
+++ scanner.py
@@ -1,54 +1,72 @@
 from config import get_config
-from time import sleep
+from database import Database
+
 import os
+from time import sleep
+from fnmatch import fnmatch
+import logging as log
 
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
-from fnmatch import fnmatch
+
+log.basicConfig(filename='scanner.log', level=log.DEBUG)
 
 
-def add_file(path):
-    print("Adding:", path)
+def add_resource(path):
+    log.debug('Adding %s'.format(path))
 
 
-def update_file(path):
-    print("Updating:", path)
+def update_resource(path):
+    log.debug('Updating %s'.format(path))
+
+
+def remove_resource(path):
+    log.debug('Removing %s'.format(path))
+
+
+class Handler(PatternMatchingEventHandler):
+    def __init__(self, db, *args, **kwargs):
+        self.db = db
+        super().__init__(*args, **kwargs)
+
+    def on_any_event(self, event):
+        update_file(event.src_path)
 
 
 class Scanner(Observer):
-    class Handler(PatternMatchingEventHandler):
-        def on_any_event(self, event):
-            print(event.src_path, event.event_type)
-            update_file(event.src_path)
-
-    def __init__(self, path, patterns):
+    def __init__(self, db, path, patterns):
         super().__init__()
 
         self.patterns = patterns
         self.path = path
 
-        handler = Scanner.Handler(patterns=patterns, ignore_directories=True)
+        handler = Handler(db, patterns=patterns, ignore_directories=True)
         self.schedule(handler, path, recursive=True)
 
     def start(self):
-        self.first_time_scan()
+        self.force_scan()
         super().start()
 
-    def first_time_scan(self):
+    def force_scan(self):
         for root, dirs, files in os.walk(path):
             for f in files:
                 for pattern in self.patterns:
                     if fnmatch(f, pattern):
-                        add_file(os.path.join(root, f))
+                        add_resource(os.path.join(root, f))
 
 
 if __name__ == "__main__":
+    log.debug('Starting scanner')
+
     conf = get_config()
     path = conf["SCANNER"]["path"]
 
-    # TODO: get patterns from config
-    scanner = Scanner(path, ["*.mp3", "*.flac", "*.ogg"])
+    log.debug('Initializing db connection')
+    db = Database()
+
+    scanner = Scanner(db, path, ["*.mp3", "*.flac", "*.ogg"])
     scanner.start()
+
     try:
         while True:
             sleep(1)
