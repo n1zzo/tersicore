40e4244f82fa8dd34c6f401e2715e473e61cd2f4
Edoardo Negri <edne@gmx.com>
2017-01-20 17:07:55 +0100

    Add resources together

--- scanner.py
+++ scanner.py
@@ -1,5 +1,5 @@
 from config import get_config
-from database import Database
+# from database import Database
 
 import os
 from time import sleep
@@ -13,15 +13,15 @@ log.basicConfig(filename='scanner.log', level=log.DEBUG)
 
 
 def add_resource(path):
-    log.debug('Adding %s'.format(path))
+    log.debug('Adding {}'.format(path))
 
 
 def update_resource(path):
-    log.debug('Updating %s'.format(path))
+    log.debug('Updating {}'.format(path))
 
 
 def remove_resource(path):
-    log.debug('Removing %s'.format(path))
+    log.debug('Removing {}'.format(path))
 
 
 class Handler(PatternMatchingEventHandler):
@@ -48,11 +48,18 @@ class Scanner(Observer):
         super().start()
 
     def force_scan(self):
-        for root, dirs, files in os.walk(path):
-            for f in files:
-                for pattern in self.patterns:
-                    if fnmatch(f, pattern):
-                        add_resource(os.path.join(root, f))
+
+        def match(f):
+            return any(fnmatch(f, pattern)
+                       for pattern in self.patterns)
+
+        matches = [os.path.join(root, f)
+                   for root, dirs, files in os.walk(path)
+                   for f in files
+                   if match(f)]
+
+        for file_name in matches:
+            add_resource(file_name)
 
 
 if __name__ == "__main__":
@@ -62,7 +69,8 @@ if __name__ == "__main__":
     path = conf["SCANNER"]["path"]
 
     log.debug('Initializing db connection')
-    db = Database()
+    # db = Database()
+    db = None
 
     scanner = Scanner(db, path, ["*.mp3", "*.flac", "*.ogg"])
     scanner.start()
