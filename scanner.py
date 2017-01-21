9f3daa3bc54fcbe7f813d2cd65f0805ed1ae3bcd
Niccolò Izzo <izzo.niccolo@gmail.com>
2017-01-21 21:10:05 +0100

    Implemented mutagen tag parsing.

--- scanner.py
+++ scanner.py
@@ -2,25 +2,56 @@ from config import get_config
 from database import Database
 
 import os
+import logging as log
 from time import sleep
 from fnmatch import fnmatch
-import logging as log
+from datetime import date
 
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
+from mutagen import File
+
 log.basicConfig(filename='scanner.log', level=log.DEBUG)
 
 
-def add_resource(path):
+def add_resource(db, path):
     log.debug('Adding {}'.format(path))
+    tag = File(path)
+
+    track = db.Track(
+        track_number=tag["tracknumber"],
+        total_tracks=tag["totaltracks"],
+        disc_number=tag["discnumber"],
+        total_discs=tag["totaldiscs"],
+        title=tag["title"],
+        artist=tag["artist"],
+        album_artist=tag["ensemble"],
+        album=tag["album"],
+        compilation=False,
+        date=date(tag["date"]),
+        label=tag["organization"],
+        isrc=tag["isrc"]
+        )
+
+    with db.get_session() as session:
+        session.add(track)
+        track.resources = [
+            db.Resource(codec=getCodec(type(tag)),
+                        bitrate=tag.info.bitrate,
+                        path=path),
+        ]
 
 
-def update_resource(path):
+def getCodec(filetype):
+    pass
+
+
+def update_resource(db, path):
     log.debug('Updating {}'.format(path))
 
 
-def remove_resource(path):
+def remove_resource(db, path):
     log.debug('Removing {}'.format(path))
 
 
@@ -66,7 +97,7 @@ class Scanner(Observer):
                 log.debug('Removing {}'.format(res))
 
         for file_name in matches:
-            add_resource(file_name)
+            add_resource(db, file_name)
 
 
 if __name__ == "__main__":
