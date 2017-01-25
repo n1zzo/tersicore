3dc4aa71efb004a01edd7eed68a7ae8b37d748aa
Niccolò Izzo <izzo.niccolo@gmail.com>
2017-01-25 17:35:25 +0100

    Fixed compatibility issue with sqlite, now we every element of the track is a single element instead of a list.

--- formats.py
+++ formats.py
@@ -40,21 +40,21 @@ def parse_resource(res, path):
     res.sample_rate = media.info.sample_rate
     res.bitrate = media.info.bitrate
 
-    res.track.track_number = media.tags.get('tracknumber', None)
-    res.track.total_tracks = media.tags.get('totaltracks', None)
-    res.track.disc_number = media.tags.get('discnumber', None)
-    res.track.total_discs = media.tags.get('totaldiscs', None)
-    res.track.label = media.tags.get('organization', None)
-    res.track.title = media.tags.get('title', None)
-    res.track.artist = media.tags.get('artist', None)
-    res.track.album = media.tags.get('album', None)
+    res.track.track_number = media.tags.get('tracknumber', None)[0]
+    res.track.total_tracks = media.tags.get('totaltracks', None)[0]
+    res.track.disc_number = media.tags.get('discnumber', None)[0]
+    res.track.total_discs = media.tags.get('totaldiscs', None)[0]
+    res.track.label = media.tags.get('organization', None)[0]
+    res.track.title = media.tags.get('title', None)[0]
+    res.track.artist = media.tags.get('artist', None)[0]
+    res.track.album = media.tags.get('album', None)[0]
     res.track.compilation = False
     res.track.date = date(int(media.tags.get('date', None)[0]), 1, 1)
-    res.track.isrc = media.tags.get('isrc', None)
+    res.track.isrc = media.tags.get('isrc', None)[0]
 
     if res.codec == 'mp3':
-        res.track.album_artist = media.tags.get('albumartist', None)
+        res.track.album_artist = media.tags.get('albumartist', None)[0]
     elif res.codec == 'ogg_vorbis':
-        res.track.album_artist = media.tags.get('ensemble', None)
+        res.track.album_artist = media.tags.get('ensemble', None)[0]
     elif res.codec == 'flac':
-        res.track.album_artist = media.tags.get('albumartist', None)
+        res.track.album_artist = media.tags.get('albumartist', None)[0]
