4f83e074f576133d37f1446cdaf1886d1e0f03db
Edoardo Negri <edne@gmx.com>
2017-03-21 11:24:03 +0100

    Move all the meiascanner code in the mediascanner file

--- mediascanner.py
+++ mediascanner.py
@@ -1,41 +1,4 @@
-import argparse
-
-from tersicore.config import Config
-from tersicore.log import init_logging
-from tersicore.database import Database
-from tersicore.mediascanner import MediaScanner
-
-
-def main():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('-d', '--config-dir')
-    parser.add_argument('--tersicore-config')
-    parser.add_argument('--logging-config')
-
-    args = parser.parse_args()
-
-    config = Config(
-        basedir=args.config_dir,
-        tersicore_file=args.tersicore_config,
-        logging_file=args.logging_config)
-
-    config_logging = config.logging
-    init_logging(config_logging)
-
-    config_database = config.tersicore['DATABASE']
-    database = Database(**config_database)
-
-    config_scanner = config.tersicore['SCANNER']
-    config_mediascanner_paths = str(config_scanner['Path']).split(',')
-    config_mediascanner_formats = str(config_scanner['Formats']).split(',')
-
-    mediascanner = MediaScanner(
-        paths=config_mediascanner_paths,
-        formats=config_mediascanner_formats,
-        database=database)
-
-    mediascanner.run()
+from tersicore.mediascanner import main
 
 
 if __name__ == '__main__':
--- tersicore/mediascanner.py
+++ tersicore/mediascanner.py
@@ -12,6 +12,12 @@ from tersicore.formats import glob_match
 from tersicore.database import Resource, Track
 from tersicore.formats import parse_resource
 
+import argparse
+from tersicore.config import Config
+from tersicore.log import init_logging
+from tersicore.database import Database
+
+
 log = get_logger('mediascanner')
 
 
@@ -115,3 +121,35 @@ class MediaScanner:
                 for f in files
                 if glob_match(f, FORMATS_GLOB)
                 ]
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-d', '--config-dir')
+    parser.add_argument('--tersicore-config')
+    parser.add_argument('--logging-config')
+
+    args = parser.parse_args()
+
+    config = Config(
+        basedir=args.config_dir,
+        tersicore_file=args.tersicore_config,
+        logging_file=args.logging_config)
+
+    config_logging = config.logging
+    init_logging(config_logging)
+
+    config_database = config.tersicore['DATABASE']
+    database = Database(**config_database)
+
+    config_scanner = config.tersicore['SCANNER']
+    config_mediascanner_paths = str(config_scanner['Path']).split(',')
+    config_mediascanner_formats = str(config_scanner['Formats']).split(',')
+
+    mediascanner = MediaScanner(
+        paths=config_mediascanner_paths,
+        formats=config_mediascanner_formats,
+        database=database)
+
+    mediascanner.run()
